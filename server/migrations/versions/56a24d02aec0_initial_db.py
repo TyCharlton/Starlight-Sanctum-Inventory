6996f0e7208e7f5b5565eb5461b57b6388e667f9
TylerCharlton <TyCharlton1993@gmail.com>
2024-02-12 13:00:24 -0500

    initial commit

---
+++ server/migrations/versions/56a24d02aec0_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 56a24d02aec0
+Revises: 
+Create Date: 2024-02-12 13:00:23.746424
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '56a24d02aec0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
