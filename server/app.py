ecb332e2db91546f70efc14c97513864a21b5e95
TylerCharlton <TyCharlton1993@gmail.com>
2024-02-14 12:35:33 -0500

    Imported rich and started CLI(Welcome message.)

--- server/app.py
+++ server/app.py
@@ -1,9 +1,14 @@
 from config import app, migrate
+from rich import print
 
 from models import db
+
+def display_welcome_message():
+  print("Welcome to my app. This will be changed.")
 
 if __name__ == "__main__":
   with app.app_context():
     migrate.init_app(app, db)
-    pass
-    # remove pass and write your cli logic
+    
+
+    display_welcome_message()
