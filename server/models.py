b293399406e15b8b83d584d523464563dc8ada24
TylerCharlton <TyCharlton1993@gmail.com>
2024-02-13 19:28:11 -0500

    Created models, added diagram.

--- server/models.py
+++ server/models.py
@@ -1,2 +1,20 @@
 from config import db
+from sqlalchemy.sql import func
 
+class Character(db.Model):
+    __tablename__ = "characters"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+class Inventory(db.Model):
+    __tablename__ = "inventories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String)
+    character_id = db.Column(db.Integer, db.ForeignKey("characters.id"))
+    created_at = db.Column(db.DateTime, server_default = func.now())
+    updated_at = db.Column(db.DateTime, onupdate = func.now())
+    
+
+    
